9cf40cb09230932d28eabc3b35de7292473e855d
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-12-02 18:07:54 +0100

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
 
+<TheHeader></TheHeader>
+
+<main></main>
+
+
 
 </template>
 
 
 <script>
+
+import TheHeader from './components/TheHeader.vue';
+
 export default {
+  components: {TheHeader,}
 
 }
 </script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,82 @@
 <template>
-  <div>
 
-  </div>
+  <header>
+
+    <nav class="navbar navbar-expand-lg bg-light py-3">
+      
+      <div class="container">
+        
+        <div>
+          <a class="navbar-brand" href="#">
+            <img src="/img/dc-logo.png" alt="logo dc comics">
+          </a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="d-none d-lg-flex">
+
+          <ul class="navbar-nav">
+
+            <li class="nav-item" v-for="(navItem) in items">
+              <a class="nav-link active fs-6" aria-current="page" href="#">{{ navItem.title }}</a>
+            </li>
+
+          </ul>
+
+        </div>
+
+      </div>
+
+    </nav>
+
+  </header>
+
 </template>
 
 
 <script>
 export default {
+
+  data() {
+    return {
+      items: [
+        {
+          title: "CHARACTERS",
+        },
+        {
+          title: "COMICS",
+        },
+        {
+          title: "MOVIES",
+        },
+        {
+          title: "TV",
+        },
+        {
+          title: "GAMES",
+        },
+        {
+          title: "COLLECTIBLES",
+        },
+        {
+          title: "VIDEOS",
+        },
+        {
+          title: "FANS",
+        },
+        {
+          title: "NEWS",
+        },
+        {
+          title: "SHOP",
+        },
+      ]
+    }
+  }
 
 }
 </script>
@@ -14,5 +84,6 @@ export default {
 
 
 <style lang="scss">
+
 
 </style>
