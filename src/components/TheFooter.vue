e46d1ee91620121b1e9ea51b41c34d2141a26701
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-12-03 10:40:14 +0100

    complete footer but not fixed

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -10,15 +10,15 @@
 
           <div class="col">
 
-            <div class="row row-cols-3">
+            <div class="row row-cols-1 g-2 row-cols-md-2 row-cols-lg-3">
 
-              <div class="col" v-for="listFooter in navMap">
+              <div class="col" v-for="list in navMap">
 
-                <h5>{{ listFooter.title }}</h5>
+                <h5>{{ list.title }}</h5>
                 <ul class="nav flex-column">
 
                   <li class="nav-item">
-                    <a class="nav-link active px-0 text-secondary" href="#">{{ listFooter.listSubtitle }}</a>
+                    <a class="nav-link active px-0 text-secondary" href="#">{{ list.subtitle }}</a>
                   </li>
 
                 </ul>
@@ -29,7 +29,8 @@
 
           </div>
 
-          <div class="d-none d-lg-flex col-lg-8">SPAZIO VUOTO !!!</div>
+          <div class="d-none d-lg-flex col-lg-8 bg-dc">
+          </div>
 
         </div>
       </div>
@@ -49,11 +50,10 @@
         <!-- Container Icon Social -->
         <div>
 
-          <ul class="nav align-items-center">
+          <ul class="nav align-items-center g-1">
 
-            <li class="nav-item">
-              <a class="nav-link px-2 fw-bold fs-5" href="#">FOLLOW US
-              </a>
+            <li class="nav-item d-none d-md-flex">
+              <a class="nav-link px-2 fw-bold fs-5" href="#">FOLLOW US</a>
             </li>
 
             <li class="text-white" v-for="iconList in icon">
@@ -83,35 +83,20 @@ export default {
       navMap: [
         {
           title: "DC COMICS",
-          listSubtitle: [
-            {
-              list: ['1', '2'],
-            }
-          ],
+          subtitle: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"],
         },
         {
           title: "DC",
-          listSubtitle: [
-            {
-              list: ['1', '2'],
-            }
-          ],
+          subtitle: ["Terms Of Use", "Privacy Police (New)", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"],
+
         },
         {
           title: "SITES",
-          listSubtitle: [
-            {
-              list: ['1', '2'],
-            }
-          ],
+          subtitle: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"],
         },
         {
           title: "SHOP",
-          listSubtitle: [
-            {
-              list: ['1', '2'],
-            }
-          ],
+          subtitle: ["Shop DC", "Shop DC Collectibles"],
         },
       ],
       icon: [
@@ -135,7 +120,7 @@ export default {
           src: "/img/footer-periscope.png",
           alt: "logo periscope"
         },
-      ]
+      ],
     }
   }
 
@@ -159,5 +144,14 @@ export default {
   border: 3px solid #0282F9;
   background-color: transparent;
   font-weight: bold;
+}
+
+.bg-dc {
+  background-image: url(/img/dc-logo-bg.png);
+  background-repeat: no-repeat;
+  background-position: right;
+  background-position-y: center;
+  background-position-y: center;
+
 }
 </style>
